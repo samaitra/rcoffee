63add05b42b164a3f0b74f5f10c49c55c86ed6b4
Saikat Maitra <saikat.maitra@gmail.com>
2012-03-24 12:23:38 +0530

    first commit with project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rcoffee::Application.config.secret_token = 'feb30be951bb81b94428d812e46b59689b740df8ccbf140e3cb30eae8291eb8dbbb049a6d8083f10f1c7fe6fd6bf36c6ea8c677701e700bd8822e152453025ee'
